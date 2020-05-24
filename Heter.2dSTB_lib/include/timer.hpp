fbc87818efeaebc65949fef6994b3a47f3bdf429
Alexander Tereshin <tereshin.tesha@gmail.com>
2020-05-24 15:00:08 +0100

    ADDED: simple convenient profiling tool is added

---
+++ Heter.2dSTB_lib/include/timer.hpp
@@ -0,0 +1,40 @@
+#ifndef H_SIMPLE_TIMER
+#define H_SIMPLE_TIMER
+
+#include <chrono>
+#include <ctime>
+#include <string>
+#include <iostream>
+
+namespace prof {
+
+class timer
+{
+public:
+    timer(){iter = 0;}
+    ~timer(){iter = 0;}
+
+    void End( std::string var )
+    {
+        auto end = std::chrono::high_resolution_clock::now();
+        auto time_span = std::chrono::duration_cast<std::chrono::microseconds>( end - start );
+        resTime = time_span.count()/1000000.0f;
+        std::cout.precision(6);
+        std::cout << var << ", iteration ["<< iter <<"] : " << resTime << " [Sec];" << std::endl;
+
+        if(iter > 1)
+            printf("\x1b[A");
+    }
+    inline float getTime() { return resTime; }
+    inline void Start() { start = std::chrono::high_resolution_clock::now(); iter++; }
+    inline void resetIter() { std::cout <<"\nlast iter: " << iter << std::endl; iter = 0; }
+private:
+    std::chrono::high_resolution_clock::time_point start;
+    int iter;
+    float resTime;
+};
+
+}
+
+
+#endif //#define H_SIMPLE_TIMER
